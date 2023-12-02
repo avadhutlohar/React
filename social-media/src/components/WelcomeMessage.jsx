7339261ca58567a80e7491a4a84bdf3db0c51960
Avadhut Lohar <loharavdhoot@gmail.com>
2023-12-02 10:25:49 +0530

    useEffect

--- social-media/src/components/WelcomeMessage.jsx
+++ social-media/src/components/WelcomeMessage.jsx
@@ -4,7 +4,7 @@ const WelcomeMessage = ({onGetPostsClick}) => {
   return (
     <center className="welcomeMessage">
       <h1 >There are no posts</h1>
-      <button type="button" onClick={onGetPostsClick} className="btn btn-primary">Get Posts from Server</button>
+      
     </center>
   );
 };
