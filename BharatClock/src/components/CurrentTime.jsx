2a120524035b161a9f92334341ebd10b50de4d1d
Avadhut Lohar <loharavdhoot@gmail.com>
2023-12-23 21:13:30 +0530

    updated

--- BharatClock/src/components/CurrentTime.jsx
+++ BharatClock/src/components/CurrentTime.jsx
@@ -1,8 +1,22 @@
-const CurrentTime =()=>{
-    let time = new Date()
-    return(
-        <p>This is the Current Time :{time.toLocaleDateString()} - {time.toLocaleTimeString()}</p>
-    )
-}
+import { useEffect } from "react";
+import { useState } from "react";
+
+const CurrentTime = () => {
+  const [time, setTime] = useState(new Date());
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+  return (
+    <p>
+      This is the Current Time : {time.toLocaleDateString()} - {" "}
+      {time.toLocaleTimeString()}
+    </p>
+  );
+};
 
 export default CurrentTime;
