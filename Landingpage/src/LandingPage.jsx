52ab352d801a132b0b879e915b876265d866ab15
Avadhut Lohar <loharavdhoot@gmail.com>
2024-02-21 19:47:14 +0530

    Day 53

--- Landingpage/src/LandingPage.jsx
+++ Landingpage/src/LandingPage.jsx
@@ -1,40 +1,45 @@
 import React from "react";
-import image from "./assets/35356-500x889.webp"
+import image from "./assets/login.jpg";
 
 const LandingPage = () => {
   return (
     <>
-    <div className="container mx-auto">
-      <header className="flex items-center justify-between py-4 px-6 bg-gray-800 text-white">
-        <div className="text-xl font-bold">Logo</div>
-        <div className="flex items-center space-x-4">
-          <button className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded">
-            Profile
-          </button>
+    {/* This is the main div */}
+      <div className=" grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
+ 
+ 
+        {/* 1st Div */}
+        <div className=" bg-gray-100 flex flex-col justify-center">
+          <form className="max-w-[400px] w-full mx-auto bg-white p-4 ">
+            <h2 className="text-4xl font-bold text-center py-6">Brand</h2>
+            <div className="flex flex-col py-2">
+              <label>Username</label>
+              <input className="border p-2 " type="text" />
+            </div>
+            <div className="flex flex-col py-2">
+              <label>Password</label>
+              <input className="border p-2 " type="password" />
+            </div>
+            <button className="border w-full my-5 py-2 bg-indigo-600 hover:bg-indigo-400 text-white"> Sign In</button>
+            <div className="flex justify-between">
+              <p className="flex items-center">
+                <input type="checkbox" className="mr-2"/>
+                Remember Me
+              </p>
+              <p>Create an account</p>
+            </div>
+          </form>
         </div>
-      </header>
-      <main className="flex flex-col ">
-        <div className="flex justify-evenly flex-row">
-          {/* Data div */}
-          <div className="mb-8 space-y-4">
-            {/* Your data components here */}
-            <p>Your data content 1</p>
-            <p>Your data content 2</p>
-          </div>
-          {/* Image div */}
-          <div className="aspect-ratio aspect-w-16 aspect-h-9" >
-            <img
-              src={image}
-              alt="Your image description"
-              className="object-cover w-full rounded"
-            />
-          </div>
+
+        {/* 2nd Div */}
+        <div className="hidden sm:block ">
+          <img
+            src={image}
+            alt="Landing Page"
+            className="w-full h-full object-cover"
+          />
         </div>
-      </main>
-      <footer className="bg-gray-800 text-white text-center py-4 px-6">
-        Copyright © 2024 Your Company Name
-      </footer>
-    </div>
+      </div>
     </>
   );
 };
