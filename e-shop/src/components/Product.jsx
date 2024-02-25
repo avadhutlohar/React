ab994e76a040934cbd772d969d1fad63ace6a70f
Avadhut Lohar <loharavdhoot@gmail.com>
2024-02-25 19:00:32 +0530

    day56

--- e-shop/src/components/Product.jsx
+++ e-shop/src/components/Product.jsx
@@ -1,24 +1,32 @@
 import React from "react";
+import "./Product.css";
 
-const Product = () => {
-  return (
-    <div>
-      <div className="product">
-        <div className="product__info">
-          <p>The lean startup</p>
-          <p className="product__price">
-            <small>$</small>
-            <strong>19.99</strong>
-          </p>
-          <div className="product__rating">⭐</div>
+
+function Product({id, title, image, price, rating}) {
+
+ 
+    return (
+        <div className="product">
+            <div className="product__info">
+                <p>{title}</p>
+                <p className="product__price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product__rating">
+                    {Array(rating)
+                    .fill()
+                    .map((_, i) => (
+                        <p>⭐</p>
+                    ))}
+                </div>
+            </div>
+
+            <img src={image} />
+
+            <button >Add to Basket</button>
         </div>
-        <img
-          src="https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt=""/>
-        <button>Add to Basket</button>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default Product;
+export default Product
